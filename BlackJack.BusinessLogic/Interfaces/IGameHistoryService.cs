16360e578f23d64d39f42ff864e5c008c10acbee
Mizuwokiru <mizuwokiru@gmail.com>
2019-03-15 16:33:54 +0200

    Added service interfaces.

---
+++ BlackJack.BusinessLogic/Interfaces/IGameHistoryService.cs
@@ -0,0 +1,18 @@
+﻿using BlackJack.BusinessLogic.DTO;
+using System.Collections.Generic;
+
+namespace BlackJack.BusinessLogic.Interfaces
+{
+    public interface IGameHistoryService
+    {
+        IEnumerable<GameDTO> GetAllGames();
+
+        IEnumerable<RoundDTO> GetRoundsByGame(int gameId);
+
+        IEnumerable<GamePlayerDTO> GetPlayersByGame(int gameId);
+
+        IEnumerable<RoundPlayerDTO> GetPlayersByRound(int roundId);
+
+        IEnumerable<RoundPlayerCardDTO> GetCardsByPlayer(int roundPlayerId);
+    }
+}
---
+++ BlackJack.BusinessLogic/Interfaces/IGameService.cs
@@ -0,0 +1,19 @@
+﻿using BlackJack.BusinessLogic.DTO;
+
+namespace BlackJack.BusinessLogic.Interfaces
+{
+    public interface IGameService
+    {
+        void MakeGame(GameDTO game);
+
+        void MakePlayer(PlayerDTO player);
+
+        void MakeRound(RoundDTO round, int gameId);
+
+        void MakeGamePlayer(GamePlayerDTO gamePlayer, int gameId);
+
+        void MakeRoundPlayer(RoundPlayerDTO roundPlayer, int roundId);
+
+        void MakeRoundPlayerCard(RoundPlayerCardDTO roundPlayerCard, int roundPlayerId);
+    }
+}
