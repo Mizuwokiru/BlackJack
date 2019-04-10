03489e4ad3590a9150aacbd0e6e928b3305861ee
Mizuwokiru <mizuwokiru@gmail.com>
2019-04-10 16:50:32 +0300

    Added link to game history.

---
+++ BlackJack.Web/Controllers/HistoryController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlackJack.Web.Controllers
+{
+    [Authorize]
+    [Route("[controller]")]
+    public class HistoryController : Controller
+    {
+        [Route("")]
+        [Route("[action]")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
