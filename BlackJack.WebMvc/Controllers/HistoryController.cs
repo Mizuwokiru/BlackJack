bfd03030785f2f8d96c7ced1b21e5fb0d97bb5f3
Mizuwokiru <mizuwokiru@gmail.com>
2019-04-24 18:23:26 +0300

    End of day.

--- BlackJack.WebMvc/Controllers/HistoryController.cs
+++ BlackJack.WebMvc/Controllers/HistoryController.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlackJack.WebMvc.Controllers
 {
+    [Authorize]
     public class HistoryController : Controller
     {
         public IActionResult Index()
